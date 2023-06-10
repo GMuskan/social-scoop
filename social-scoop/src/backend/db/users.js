39378b5eea31839c6c065a9a90960cc55330e44a
GMuskan <muskaang710@gmail.com>
2023-06-10 09:16:42 +0530

    feat: changed backend users and posts data

--- social-scoop/src/backend/db/users.js
+++ social-scoop/src/backend/db/users.js
@@ -4,15 +4,131 @@ import { formatDate } from "../utils/authUtils";
  * User Database can be added here.
  * You can add default users of your wish with different attributes
  * */
-
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    username: "adarshbalika",
-    password: "adarshBalika123",
+    fullName: "Muskan Gupta",
+    username: "muskan09",
+    password: "123",
+    bio: "Hey there, Muskan here",
+    website: "https://github.com/percius47",
+    profileAvatar:
+      "https://picsum.photos/id/1012/150",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    following: [
+      {
+        _id: uuid(), fullName: "John Doe", username: "johndoe", profileAvatar:
+          "https://picsum.photos/id/1009/150"
+      },
+
+
+    ],
+    followers: [
+      {
+        _id: uuid(), fullName: "John Doe", username: "johndoe", profileAvatar:
+          "https://picsum.photos/id/1009/150"
+      },
+      {
+        _id: uuid(), fullName: "Aditya Jadhav", username: "aditya_jadhav", profileAvatar:
+          "https://picsum.photos/id/100/150"
+      },
+      {
+        _id: uuid(), fullName: "Anshaal Khanna", username: "anshaal10", profileAvatar:
+          "https://picsum.photos/id/1005/150"
+      },
+    ],
+  },
+  {
+    _id: uuid(),
+    fullName: "John Doe",
+    username: "johndoe",
+    password: "123john",
+    bio: "Hey there, John here",
+    website: "https://www.johndoe.com",
+    profileAvatar:
+      "https://picsum.photos/id/1009/150",
+    createdAt: "2022-05-05",
+    updatedAt: formatDate(),
+    following: [
+      {
+        _id: uuid(), fullName: "Muskan Gupta", username: "muskan09", profileAvatar:
+          "https://picsum.photos/id/1012/150",
+      },
+      {
+        _id: uuid(), fullName: "Aditya Jadhav", username: "aditya_jadhav", profileAvatar:
+          "https://picsum.photos/id/100/150"
+      },
+    ],
+    followers: [
+      {
+        _id: uuid(), fullName: "Anshaal Khanna", username: "anshaal10", profileAvatar:
+          "https://picsum.photos/id/1005/150"
+      },
+      {
+        _id: uuid(), fullName: "Muskan Gupta", username: "muskan09", profileAvatar:
+          "https://picsum.photos/id/1012/150",
+      }
+    ],
+  },
+  {
+    _id: uuid(),
+    fullName: "Anshaal Khanna",
+    username: "anshaal10",
+    password: "anshaal123",
+    bio: "Hey there, Anshaal here",
+    website: "https://www.nike.com",
+    profileAvatar:
+      "https://picsum.photos/id/1005/150",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    following: [
+      {
+        _id: uuid(), fullName: "Muskan Gupta", username: "muskan09", profileAvatar:
+          "https://picsum.photos/id/1012/150",
+      },
+      {
+        _id: uuid(), fullName: "John Doe", username: "johndoe", profileAvatar:
+          "https://picsum.photos/id/1009/150"
+      },
+    ],
+    followers: [
+      {
+        _id: uuid(), fullName: "Aditya Jadhav", username: "aditya_jadhav", profileAvatar:
+          "https://picsum.photos/id/100/150"
+      },
+    ],
+  },
+  {
+    _id: uuid(),
+    fullName: "Aditya Jadhav",
+    username: "aditya_jadhav",
+    password: "jadhav123",
+    bio: "Hello Fritter, Jadhav here!",
+    website: "https://www.wikipedia.com",
+    profileAvatar:
+      "https://picsum.photos/id/100/150",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    following: [
+      {
+        _id: uuid(), fullName: "Muskan Gupta", username: "muskan09", profileAvatar:
+          "https://picsum.photos/id/1012/150",
+      },
+      {
+        _id: uuid(), fullName: "Anshaal Khanna", username: "anshaal10", profileAvatar:
+          "https://picsum.photos/id/1005/150"
+      },
+    ],
+    followers: [
+      {
+        _id: uuid(), fullName: "John Doe", username: "johndoe", profileAvatar:
+          "https://picsum.photos/id/1009/150"
+      },
+      {
+        _id: uuid(), fullName: "Anshaal Khanna", username: "anshaal10", profileAvatar:
+          "https://picsum.photos/id/1005/150"
+      },
+    ],
   },
 ];
