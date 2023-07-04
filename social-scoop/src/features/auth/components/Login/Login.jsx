e9ced2b6a466d09aca8040ca7f0e55f95ef32b2b
GMuskan <muskaang710@gmail.com>
2023-07-04 15:33:09 +0530

    updated CSS

--- social-scoop/src/features/auth/components/Login/Login.jsx
+++ social-scoop/src/features/auth/components/Login/Login.jsx
@@ -4,6 +4,7 @@ import { LoginClickHandler } from "../../../../services/authService";
 import { useContext } from "react";
 import { authContext } from "../../../../Context/AuthContext";
 import { Helmet } from "react-helmet"
+import loginImg from "../../../../Assests/tablet-login.avif"
 import "./Login.css"
 export const Login = () => {
 
@@ -19,44 +20,48 @@ export const Login = () => {
         setLogin({ ...login, input: { ...login.input, [name]: value } })
     }
     return (
+        <div className="login-page">
+            <div className="login-image">
+                <img src={loginImg} alt="login-icon" />
+            </div>
+            <div className="login-container">
+                <Helmet>
+                    <title>
+                        Login | Social-Scoop
+                    </title>
+                </Helmet>
 
-        <div className="login-container">
-            <Helmet>
-                <title>
-                    Login | Social-Scoop
-                </title>
-            </Helmet>
-            <div className="login-form">
-                <div className="app-name-section">
-                    <i className="fa fa-cutlery" aria-hidden="true"></i>
-                    <article>Social Scoop</article>
-                </div>
-                <div className="about-app-section">
-                    <span>Social media for Foodies!</span>
-                </div>
-                <div className="login">
-                    <h2>Log In</h2>
-                </div>
-                <div className="login-credentials">
-                    <form
-                        autoComplete="off"
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            LoginClickHandler(login, navigate, authDispatch)
-                        }}>
-                        <div className="login-username">
-                            <label htmlFor="username">Username<span>*</span></label>
-                            <input
-                                type="text"
-                                name="username"
-                                id="username"
-                                value={login?.input?.username || ""}
-                                onChange={loginInputHandler}
-                            />
-                        </div>
-                        <div className="login-password">
-                            <label htmlFor="password">Password<span>*</span></label>
-                            <span className="login-password-span">
+                <div className="login-form">
+                    <div className="app-name-section">
+                        {/* <i className="fa fa-cutlery" aria-hidden="true"></i> */}
+                        <article>Social Scoop</article>
+                    </div>
+                    {/* <div className="about-app-section">
+                        <span>Social media for Foodies!</span>
+                    </div> */}
+                    <div className="login">
+                        <h3>Login</h3>
+                    </div>
+                    <div className="login-credentials">
+                        <form
+                            autoComplete="off"
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                LoginClickHandler(login, navigate, authDispatch)
+                            }}>
+                            <div className="login-username">
+                                <label htmlFor="username">Username<span style={{ color: "red" }}>*</span></label>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    id="username"
+                                    value={login?.input?.username || ""}
+                                    onChange={loginInputHandler}
+                                />
+                            </div>
+                            <div className="login-password">
+                                <label htmlFor="password">Password<span style={{ color: "red" }}>*</span></label>
+                                {/* <span className="login-password-span"> */}
                                 <input
                                     type={login?.hide ? "password" : "text"}
                                     name="password"
@@ -64,29 +69,32 @@ export const Login = () => {
                                     value={login?.input?.password || ""}
                                     onChange={loginInputHandler}
                                 />
-                                <i className={login?.hide ? "fa fa-eye-slash" : "fa fa-eye"} onClick={() => setLogin({
+                                <span className="login-password-span">
+                                    <i className={login?.hide ? "fa fa-eye-slash" : "fa fa-eye"} onClick={() => setLogin({
+                                        ...login,
+                                        hide: !login.hide
+                                    })}></i>
+                                </span>
+                                {/* </span> */}
+                            </div>
+                            <div className="login-buttons">
+                                <button className="login-btn" type="submit">Login</button>
+                                <button className="guest-login-btn" type="submit" onClick={() => setLogin({
                                     ...login,
-                                    hide: !login.hide
-                                })}></i>
-                            </span>
-                        </div>
-                        <div className="login-buttons">
-                            <button className="login-btn" type="submit">Login</button>
-                            <button className="guest-login-btn" type="submit" onClick={() => setLogin({
-                                ...login,
-                                input: {
-                                    username: "muskan09",
-                                    password: "muskan#123"
-                                }
-                            })}>Guest Mode</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="signup-section">
-                    <span>Don't have an account?</span>
-                    <Link to="/signup">
-                        Signup
-                    </Link>
+                                    input: {
+                                        username: "muskan09",
+                                        password: "muskan#123"
+                                    }
+                                })}>Guest Mode</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="signup-section">
+                        <span>Don't have an account?</span>
+                        <Link className="signup-link" to="/signup">
+                            Signup
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
--- social-scoop/src/features/auth/components/SignUp/SignUp.jsx
+++ social-scoop/src/features/auth/components/SignUp/SignUp.jsx
@@ -5,11 +5,13 @@ import { authContext } from "../../../../Context/AuthContext";
 import { Helmet } from "react-helmet";
 import "./SignUp.css"
 import { feedContext } from "../../../../Context/FeedContext";
+import signupImg from "../../../../Assests/sign-up-concept.avif"
+
 
 export const SignUp = () => {
 
     const { authDispatch, navigate } = useContext(authContext);
-    const {feedDispatch, feedState} = useContext(feedContext)
+    const { feedDispatch, feedState } = useContext(feedContext)
 
     const [signUp, setSignUp] = useState({
         input: {},
@@ -30,96 +32,103 @@ export const SignUp = () => {
         }
     }
     return (
-        <div className="signup-container">
-            <Helmet>
-                <title>
-                    SignUp | Social-Scoop
-                </title>
-            </Helmet>
-            <div className="signup-form">
-                <div className="app-name-section">
-                    <i className="fa fa-cutlery" aria-hidden="true"></i>
-                    <article>Social Scoop</article>
-                </div>
-                <div className="about-app-section">
-                    <span>Social media for Foodies!</span>
-                </div>
-                <div className="signup">
-                    <h2>Sign Up</h2>
-                </div>
-                <div className="signup-credentials">
-                    <form autoComplete="off"
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            SignUpClickHandler(signUp, navigate, authDispatch, feedDispatch, feedState)
-                        }}>
-                        <div className="signup-fullName">
-                            <label htmlFor="fullName">Full Name<span>*</span></label>
-                            <input
-                                type="text"
-                                name="fullName"
-                                id="fullName"
-                                value={signUp?.input?.fullName || ""}
-                                onChange={signUpInputHandler}
-                            />
-                        </div>
-                        <div className="signup-username">
-                            <label htmlFor="username">Username<span>*</span></label>
-                            <input
-                                type="text"
-                                name="username"
-                                id="username"
-                                value={signUp?.input?.username || ""}
-                                onChange={signUpInputHandler}
-                            />
-                        </div>
-                        <div className="signup-password">
-                            <label htmlFor="password">Password<span>*</span></label>
-                            <span className="signup-password-span">
+        <div className="signup-page">
+            <div className="signup-image">
+                <img src={signupImg} alt="signup-icon" />
+            </div>
+            <div className="signup-container">
+                <Helmet>
+                    <title>
+                        SignUp | Social-Scoop
+                    </title>
+                </Helmet>
+                <div className="signup-form">
+                    <div className="app-name-section">
+                        {/* <i className="fa fa-cutlery" aria-hidden="true"></i> */}
+                        <article>Social Scoop</article>
+                    </div>
+                    {/* <div className="about-app-section">
+                        <span>Social media for Foodies!</span>
+                    </div> */}
+                    <div className="signup">
+                        <h3>Signup</h3>
+                    </div>
+                    <div className="signup-credentials">
+                        <form autoComplete="off"
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                SignUpClickHandler(signUp, navigate, authDispatch, feedDispatch, feedState)
+                            }}>
+                            <div className="signup-fullName">
+                                <label htmlFor="fullName">Full Name<span>*</span></label>
                                 <input
-                                    type={signUp?.hide?.password ? "password" : "text"}
-                                    name="password"
-                                    id="password"
-                                    value={signUp?.input?.password || ""}
+                                    type="text"
+                                    name="fullName"
+                                    id="fullName"
+                                    value={signUp?.input?.fullName || ""}
                                     onChange={signUpInputHandler}
                                 />
-                                <i className={signUp?.hide?.password ? "fa fa-eye-slash" : "fa fa-eye"} onClick={() => setSignUp({
-                                    ...signUp,
-                                    hide: { ...signUp?.hide, password: !signUp.hide?.password }
-                                })}></i>
-                            </span>
-                        </div>
-                        <div className="confirm-password">
-                            <div className="signup-confirmPassword">
-                                <label htmlFor="confirm-password">Confirm Password<span>*</span></label>
-                                <span className="signup-confirmPassword-span">
+                            </div>
+                            <div className="signup-username">
+                                <label htmlFor="username">Username<span>*</span></label>
+                                <input
+                                    type="text"
+                                    name="username"
+                                    id="username"
+                                    value={signUp?.input?.username || ""}
+                                    onChange={signUpInputHandler}
+                                />
+                            </div>
+                            <div className="signup-password">
+                                <label htmlFor="password">Password<span>*</span></label>
+                                
                                     <input
-                                        type={signUp?.hide?.confirmPassword ? "password" : "text"}
-                                        name="confirmPassword"
-                                        id="confirmPassword"
-                                        value={signUp?.input?.confirmPassword || ""}
+                                        type={signUp?.hide?.password ? "password" : "text"}
+                                        name="password"
+                                        id="password"
+                                        value={signUp?.input?.password || ""}
                                         onChange={signUpInputHandler}
-                                    />
-                                    <i className={signUp?.hide?.confirmPassword ? "fa fa-eye-slash" : "fa fa-eye"} onClick={() => setSignUp({
+                                />
+                                <span className="signup-password-span">
+                                    <i className={signUp?.hide?.password ? "fa fa-eye-slash" : "fa fa-eye"} onClick={() => setSignUp({
                                         ...signUp,
-                                        hide: { ...signUp?.hide, confirmPassword: !signUp.hide?.confirmPassword }
+                                        hide: { ...signUp?.hide, password: !signUp.hide?.password }
                                     })}></i>
                                 </span>
                             </div>
-                            {!signUp?.passwordMatch ? (
-                                <div className="pwd-not-matching">Password do not Match</div>
-                            ) : null}
-                        </div>
-                        <div>
-                            <button className="signup-btn" type="submit" disabled={!signUp?.passwordMatch}>Create Account</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="login-section">
-                    <span>Already have an account?</span>
-                    <Link to="/">
-                        Login
-                    </Link>
+                            <div className="confirm-password">
+                                <div className="signup-confirmPassword">
+                                    <label htmlFor="confirm-password">Confirm Password<span>*</span></label>
+                                    
+                                        <input
+                                            type={signUp?.hide?.confirmPassword ? "password" : "text"}
+                                            name="confirmPassword"
+                                            id="confirmPassword"
+                                            value={signUp?.input?.confirmPassword || ""}
+                                            onChange={signUpInputHandler}
+                                    />
+                                    <span className="signup-confirmPassword-span">
+                                        <i className={signUp?.hide?.confirmPassword ? "fa fa-eye-slash" : "fa fa-eye"} onClick={() => setSignUp({
+                                            ...signUp,
+                                            hide: { ...signUp?.hide, confirmPassword: !signUp.hide?.confirmPassword }
+                                        })}></i>
+                                    </span>
+                                </div>
+                                {!signUp?.passwordMatch ? (
+                                    <div className="pwd-not-matching">Password do not Match</div>
+                                ) : null}
+                            </div>
+                            <div>
+                                <button className="signup-btn" type="submit" disabled={!signUp?.passwordMatch}>Create Account</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="login-section">
+                        <span>Already have an account?</span>
+                        <Link className="login-link" to="/">
+                            Login
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
